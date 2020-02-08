da3c0be081fac65d8f4e9d4676296290595d5177
Steven Allen <steven@stebalien.com>
2020-02-07 18:24:54 -0800

    v2.3

--- child.go
+++ child.go
@@ -1,3 +1,11 @@
 package child
 
+import (
+	v3 "github.com/Stebalien/go-mod-play-dep/v3"
+)
+
 var Thing = "2.1.0"
+
+func init() {
+	v3.Thing += Thing
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Stebalien/go-mod-play-dep/v2
 
 go 1.13
+
+require github.com/Stebalien/go-mod-play-dep/v3 v3.0.0
